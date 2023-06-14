850917baf465fa7c13955e78adb618f1f1b59e5d
yuma14 <yuma140902@gmail.com>
2023-06-15 01:54:04 +0900

    TraditionalBeauty v2を追加
    透明な部分があるテクスチャで、端が極端に明るくなってしまう問題を修正

--- MCResourcePackUtil/Forms/FilterForm.Designer.cs
+++ MCResourcePackUtil/Forms/FilterForm.Designer.cs
@@ -27,195 +27,196 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.selectionBtn = new System.Windows.Forms.Button();
-			this.pathBox = new System.Windows.Forms.TextBox();
-			this.runBtn = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.progressBar = new System.Windows.Forms.ProgressBar();
-			this.label2 = new System.Windows.Forms.Label();
-			this.outputPathBox = new System.Windows.Forms.TextBox();
-			this.outputSelectBtn = new System.Windows.Forms.Button();
-			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
-			this.comboBox1 = new System.Windows.Forms.ComboBox();
-			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.filterTypeBox = new System.Windows.Forms.ComboBox();
-			this.label3 = new System.Windows.Forms.Label();
-			this.groupBox1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// selectionBtn
-			// 
-			this.selectionBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.selectionBtn.Location = new System.Drawing.Point(343, 12);
-			this.selectionBtn.Name = "selectionBtn";
-			this.selectionBtn.Size = new System.Drawing.Size(22, 19);
-			this.selectionBtn.TabIndex = 0;
-			this.selectionBtn.Text = "…";
-			this.selectionBtn.UseVisualStyleBackColor = true;
-			this.selectionBtn.Click += new System.EventHandler(this.selectionBtn_Click);
-			// 
-			// pathBox
-			// 
-			this.pathBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.selectionBtn = new System.Windows.Forms.Button();
+            this.pathBox = new System.Windows.Forms.TextBox();
+            this.runBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outputPathBox = new System.Windows.Forms.TextBox();
+            this.outputSelectBtn = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.filterTypeBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // selectionBtn
+            // 
+            this.selectionBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectionBtn.Location = new System.Drawing.Point(343, 12);
+            this.selectionBtn.Name = "selectionBtn";
+            this.selectionBtn.Size = new System.Drawing.Size(22, 19);
+            this.selectionBtn.TabIndex = 0;
+            this.selectionBtn.Text = "…";
+            this.selectionBtn.UseVisualStyleBackColor = true;
+            this.selectionBtn.Click += new System.EventHandler(this.selectionBtn_Click);
+            // 
+            // pathBox
+            // 
+            this.pathBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.pathBox.Location = new System.Drawing.Point(53, 12);
-			this.pathBox.Name = "pathBox";
-			this.pathBox.ReadOnly = true;
-			this.pathBox.Size = new System.Drawing.Size(284, 19);
-			this.pathBox.TabIndex = 1;
-			// 
-			// runBtn
-			// 
-			this.runBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.runBtn.Location = new System.Drawing.Point(290, 233);
-			this.runBtn.Name = "runBtn";
-			this.runBtn.Size = new System.Drawing.Size(75, 23);
-			this.runBtn.TabIndex = 2;
-			this.runBtn.Text = "実行(&R)";
-			this.runBtn.UseVisualStyleBackColor = true;
-			this.runBtn.Click += new System.EventHandler(this.runBtn_Click);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 15);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(35, 12);
-			this.label1.TabIndex = 3;
-			this.label1.Text = "入力: ";
-			// 
-			// progressBar
-			// 
-			this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.pathBox.Location = new System.Drawing.Point(53, 12);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.ReadOnly = true;
+            this.pathBox.Size = new System.Drawing.Size(284, 19);
+            this.pathBox.TabIndex = 1;
+            // 
+            // runBtn
+            // 
+            this.runBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.runBtn.Location = new System.Drawing.Point(290, 233);
+            this.runBtn.Name = "runBtn";
+            this.runBtn.Size = new System.Drawing.Size(75, 23);
+            this.runBtn.TabIndex = 2;
+            this.runBtn.Text = "実行(&R)";
+            this.runBtn.UseVisualStyleBackColor = true;
+            this.runBtn.Click += new System.EventHandler(this.runBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "入力: ";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.progressBar.Location = new System.Drawing.Point(12, 233);
-			this.progressBar.Name = "progressBar";
-			this.progressBar.Size = new System.Drawing.Size(272, 23);
-			this.progressBar.TabIndex = 4;
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(12, 40);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(35, 12);
-			this.label2.TabIndex = 5;
-			this.label2.Text = "出力: ";
-			// 
-			// outputPathBox
-			// 
-			this.outputPathBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.progressBar.Location = new System.Drawing.Point(12, 233);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(272, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "出力: ";
+            // 
+            // outputPathBox
+            // 
+            this.outputPathBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.outputPathBox.Location = new System.Drawing.Point(53, 37);
-			this.outputPathBox.Name = "outputPathBox";
-			this.outputPathBox.ReadOnly = true;
-			this.outputPathBox.Size = new System.Drawing.Size(284, 19);
-			this.outputPathBox.TabIndex = 6;
-			// 
-			// outputSelectBtn
-			// 
-			this.outputSelectBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.outputSelectBtn.Location = new System.Drawing.Point(343, 37);
-			this.outputSelectBtn.Name = "outputSelectBtn";
-			this.outputSelectBtn.Size = new System.Drawing.Size(22, 19);
-			this.outputSelectBtn.TabIndex = 7;
-			this.outputSelectBtn.Text = "…";
-			this.outputSelectBtn.UseVisualStyleBackColor = true;
-			this.outputSelectBtn.Click += new System.EventHandler(this.outputSelectBtn_Click);
-			// 
-			// checkedListBox1
-			// 
-			this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.outputPathBox.Location = new System.Drawing.Point(53, 37);
+            this.outputPathBox.Name = "outputPathBox";
+            this.outputPathBox.ReadOnly = true;
+            this.outputPathBox.Size = new System.Drawing.Size(284, 19);
+            this.outputPathBox.TabIndex = 6;
+            // 
+            // outputSelectBtn
+            // 
+            this.outputSelectBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputSelectBtn.Location = new System.Drawing.Point(343, 37);
+            this.outputSelectBtn.Name = "outputSelectBtn";
+            this.outputSelectBtn.Size = new System.Drawing.Size(22, 19);
+            this.outputSelectBtn.TabIndex = 7;
+            this.outputSelectBtn.Text = "…";
+            this.outputSelectBtn.UseVisualStyleBackColor = true;
+            this.outputSelectBtn.Click += new System.EventHandler(this.outputSelectBtn_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-			this.checkedListBox1.Enabled = false;
-			this.checkedListBox1.FormattingEnabled = true;
-			this.checkedListBox1.Items.AddRange(new object[] {
+            this.checkedListBox1.Enabled = false;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
             "Blocks",
             "Items",
             "Entities",
             "Gui",
             "その他(assets内)",
             "その他(assets外)"});
-			this.checkedListBox1.Location = new System.Drawing.Point(6, 18);
-			this.checkedListBox1.Name = "checkedListBox1";
-			this.checkedListBox1.Size = new System.Drawing.Size(120, 88);
-			this.checkedListBox1.TabIndex = 8;
-			// 
-			// comboBox1
-			// 
-			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.comboBox1.Enabled = false;
-			this.comboBox1.FormattingEnabled = true;
-			this.comboBox1.Items.AddRange(new object[] {
+            this.checkedListBox1.Location = new System.Drawing.Point(6, 18);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 88);
+            this.checkedListBox1.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
             "短辺16px以下",
             "短辺32px以下",
             "短辺64px以下",
             "正方形のみ",
             "すべて"});
-			this.comboBox1.Location = new System.Drawing.Point(132, 18);
-			this.comboBox1.Name = "comboBox1";
-			this.comboBox1.Size = new System.Drawing.Size(215, 20);
-			this.comboBox1.TabIndex = 9;
-			// 
-			// groupBox1
-			// 
-			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.comboBox1.Location = new System.Drawing.Point(132, 18);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(215, 20);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.groupBox1.Controls.Add(this.checkedListBox1);
-			this.groupBox1.Controls.Add(this.comboBox1);
-			this.groupBox1.Enabled = false;
-			this.groupBox1.Location = new System.Drawing.Point(12, 99);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(353, 128);
-			this.groupBox1.TabIndex = 10;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "処理対象のテクスチャ";
-			// 
-			// filterTypeBox
-			// 
-			this.filterTypeBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.groupBox1.Controls.Add(this.checkedListBox1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Location = new System.Drawing.Point(12, 99);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(353, 128);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "処理対象のテクスチャ";
+            // 
+            // filterTypeBox
+            // 
+            this.filterTypeBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.filterTypeBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.filterTypeBox.FormattingEnabled = true;
-			this.filterTypeBox.Items.AddRange(new object[] {
+            this.filterTypeBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.filterTypeBox.FormattingEnabled = true;
+            this.filterTypeBox.Items.AddRange(new object[] {
             "TraditionalBeauty",
-            "TraditionalBeauty MoreBold"});
-			this.filterTypeBox.Location = new System.Drawing.Point(72, 62);
-			this.filterTypeBox.Name = "filterTypeBox";
-			this.filterTypeBox.Size = new System.Drawing.Size(293, 20);
-			this.filterTypeBox.TabIndex = 11;
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(12, 65);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(54, 12);
-			this.label3.TabIndex = 12;
-			this.label3.Text = "フィルター: ";
-			// 
-			// FilterForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(377, 268);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.filterTypeBox);
-			this.Controls.Add(this.groupBox1);
-			this.Controls.Add(this.outputSelectBtn);
-			this.Controls.Add(this.outputPathBox);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.progressBar);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.runBtn);
-			this.Controls.Add(this.pathBox);
-			this.Controls.Add(this.selectionBtn);
-			this.Name = "FilterForm";
-			this.Text = "フィルター - MCResourcePackUtil";
-			this.groupBox1.ResumeLayout(false);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            "TraditionalBeauty MoreBold",
+            "TraditionalBeauty v2"});
+            this.filterTypeBox.Location = new System.Drawing.Point(72, 62);
+            this.filterTypeBox.Name = "filterTypeBox";
+            this.filterTypeBox.Size = new System.Drawing.Size(293, 20);
+            this.filterTypeBox.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "フィルター: ";
+            // 
+            // FilterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 268);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.filterTypeBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.outputSelectBtn);
+            this.Controls.Add(this.outputPathBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.runBtn);
+            this.Controls.Add(this.pathBox);
+            this.Controls.Add(this.selectionBtn);
+            this.Name = "FilterForm";
+            this.Text = "フィルター - MCResourcePackUtil";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
--- MCResourcePackUtil/Forms/FilterForm.cs
+++ MCResourcePackUtil/Forms/FilterForm.cs
@@ -17,116 +17,131 @@ using System.Windows.Forms;
 
 namespace MCResourcePackUtil.Forms
 {
-	public partial class FilterForm : Form
-	{
-		private CommonSaveFileDialog zipSaveDialog;
-		private CommonOpenFileDialog folderSaveDialog;
+    public partial class FilterForm : Form
+    {
+        private CommonSaveFileDialog zipSaveDialog;
+        private CommonOpenFileDialog folderSaveDialog;
 
-		private List<string> resourcePackPaths;
-		private ResourcePackType resourcePackType;
+        private List<string> resourcePackPaths;
+        private ResourcePackType resourcePackType;
 
-		private string outputZipPath;
-		private string outputFolderPath;
+        private string outputZipPath;
+        private string outputFolderPath;
 
-		public FilterForm()
-		{
-			InitializeComponent();
+        public FilterForm()
+        {
+            InitializeComponent();
 
-			this.zipSaveDialog = new CommonSaveFileDialog("保存先を選択...");
-			this.zipSaveDialog.Filters.Add(new CommonFileDialogFilter("Zipファイル", "*.zip"));
-			this.zipSaveDialog.Filters.Add(new CommonFileDialogFilter("すべてのファイル", "*.*"));
+            this.zipSaveDialog = new CommonSaveFileDialog("保存先を選択...");
+            this.zipSaveDialog.Filters.Add(new CommonFileDialogFilter("Zipファイル", "*.zip"));
+            this.zipSaveDialog.Filters.Add(new CommonFileDialogFilter("すべてのファイル", "*.*"));
 
-			this.folderSaveDialog = new CommonOpenFileDialog("保存先フォルダを選択...");
-			this.folderSaveDialog.IsFolderPicker = true;
+            this.folderSaveDialog = new CommonOpenFileDialog("保存先フォルダを選択...");
+            this.folderSaveDialog.IsFolderPicker = true;
 
-			this.filterTypeBox.SelectedIndex = 0;
-		}
-
-
-		private void selectionBtn_Click(object sender, EventArgs e)
-		{
-			var dialog = new ResourcePackSelector();
-			if(dialog.ShowDialog()) {
-				this.pathBox.Text = string.Join(", ", dialog.ResourcePackPaths);
-				this.resourcePackPaths = dialog.ResourcePackPaths;
-				this.resourcePackType = dialog.ResourcePackType;
-			}
-		}
-
-		private void outputSelectBtn_Click(object sender, EventArgs e)
-		{
-			if(this.resourcePackPaths.Count == 0) {
-				MessageBox.Show("先に入力を選択してください", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				return;
-			}
-			else if (this.resourcePackPaths.Count == 1) {
-				this.zipSaveDialog.DefaultFileName = Path.GetFileNameWithoutExtension(this.resourcePackPaths[0]) + ".zip";
-				if (this.zipSaveDialog.ShowDialog() == CommonFileDialogResult.Ok) {
-					this.outputPathBox.Text = this.zipSaveDialog.FileName;
-					this.outputZipPath = this.zipSaveDialog.FileName;
-				}
-			}
-			else {
-				if(this.folderSaveDialog.ShowDialog() == CommonFileDialogResult.Ok) {
-					this.outputPathBox.Text = this.folderSaveDialog.FileName;
-					this.outputFolderPath = this.folderSaveDialog.FileName;
-				}
-			}
-		}
+            this.filterTypeBox.SelectedIndex = 0;
+        }
 
 
-		private void runBtn_Click(object sender, EventArgs e)
-		{
-			if (string.IsNullOrEmpty(this.pathBox.Text)) {
-				MessageBox.Show("リソースパックが選択されていません", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				return;
-			}
-			if (string.IsNullOrEmpty(this.outputPathBox.Text)) {
-				MessageBox.Show("出力先が選択されていません", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				return;
-			}
+        private void selectionBtn_Click(object sender, EventArgs e)
+        {
+            var dialog = new ResourcePackSelector();
+            if (dialog.ShowDialog())
+            {
+                this.pathBox.Text = string.Join(", ", dialog.ResourcePackPaths);
+                this.resourcePackPaths = dialog.ResourcePackPaths;
+                this.resourcePackType = dialog.ResourcePackType;
+            }
+        }
 
-			var filter = this.filterTypeBox.SelectedIndex == 1 
-				? (IGraphicFilter)TraditionalBeautyFilterMoreBold.Instance 
-				: (IGraphicFilter)TraditionalBeautyFilter.Instance;
+        private void outputSelectBtn_Click(object sender, EventArgs e)
+        {
+            if (this.resourcePackPaths.Count == 0)
+            {
+                MessageBox.Show("先に入力を選択してください", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if (this.resourcePackPaths.Count == 1)
+            {
+                this.zipSaveDialog.DefaultFileName = Path.GetFileNameWithoutExtension(this.resourcePackPaths[0]) + ".zip";
+                if (this.zipSaveDialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    this.outputPathBox.Text = this.zipSaveDialog.FileName;
+                    this.outputZipPath = this.zipSaveDialog.FileName;
+                }
+            }
+            else
+            {
+                if (this.folderSaveDialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    this.outputPathBox.Text = this.folderSaveDialog.FileName;
+                    this.outputFolderPath = this.folderSaveDialog.FileName;
+                }
+            }
+        }
 
-			if (this.resourcePackPaths.Count == 1) {
-				run(this.resourcePackType, this.resourcePackPaths[0], outputZipPath, filter);
-			}
-			else {
-				foreach (string pack in this.resourcePackPaths) {
-					string output = Path.Combine(this.outputFolderPath, Path.GetFileNameWithoutExtension(pack) + ".zip");
-					run(this.resourcePackType, pack, output, filter);
-				}
-			}
-			
-		}
 
-		private static void run(EnumResourcePackType packType, string packPath, string outputZipPath, IGraphicFilter filter)
-		{
-			using var tempDir = new TemporaryDirectory();
-			Debug.WriteLine($"tempDir: {tempDir.Path}");
-			var resourcePack = ResourcePackLoaders.Load(packType, packPath);
-			foreach (var texture in resourcePack.EnumerateTextures()) {
-				texture.LoadPngFileToMemory();
-				texture.ApplyFilter(filter);
-				texture.WriteToResourcePack(tempDir.Path);
-				texture.Dispose();
-			}
+        private void runBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.pathBox.Text))
+            {
+                MessageBox.Show("リソースパックが選択されていません", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(this.outputPathBox.Text))
+            {
+                MessageBox.Show("出力先が選択されていません", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
 
-			ZipFile.CreateFromDirectory(tempDir.Path, outputZipPath, CompressionLevel.Fastest, false, Encoding.UTF8);
-			using (var ar = ZipFile.Open(outputZipPath, ZipArchiveMode.Update)) {
-				var entry = ar.CreateEntry("pack.mcmeta");
-				using (var writer = new StreamWriter(entry.Open(), Encoding.UTF8)) {
-					writer.WriteLine(
-						"{\n" +
-						"  \"pack\": {\n" +
-						"    \"pack_format\": 2,\n" +
-						"    \"description\": \"Made by MCResourcePackUtil (dev: yuma140902)\"\n" +
-						"  }\n" +
-						"}");
-				}
-			}
-		}
-	}
+            var filter = this.filterTypeBox.SelectedIndex == 0 ? (IGraphicFilter)TraditionalBeautyFilter.Instance :
+                         this.filterTypeBox.SelectedIndex == 1 ? (IGraphicFilter)TraditionalBeautyFilterMoreBold.Instance :
+                         this.filterTypeBox.SelectedIndex == 2 ? (IGraphicFilter)TraditionalBeautyV2Filter.Instance :
+                         (IGraphicFilter)TraditionalBeautyFilter.Instance;
+
+            if (this.resourcePackPaths.Count == 1)
+            {
+                run(this.resourcePackType, this.resourcePackPaths[0], outputZipPath, filter);
+            }
+            else
+            {
+                foreach (string pack in this.resourcePackPaths)
+                {
+                    string output = Path.Combine(this.outputFolderPath, Path.GetFileNameWithoutExtension(pack) + ".zip");
+                    run(this.resourcePackType, pack, output, filter);
+                }
+            }
+
+        }
+
+        private static void run(EnumResourcePackType packType, string packPath, string outputZipPath, IGraphicFilter filter)
+        {
+            using var tempDir = new TemporaryDirectory();
+            Debug.WriteLine($"tempDir: {tempDir.Path}");
+            var resourcePack = ResourcePackLoaders.Load(packType, packPath);
+            foreach (var texture in resourcePack.EnumerateTextures())
+            {
+                texture.LoadPngFileToMemory();
+                texture.ApplyFilter(filter);
+                texture.WriteToResourcePack(tempDir.Path);
+                texture.Dispose();
+            }
+
+            ZipFile.CreateFromDirectory(tempDir.Path, outputZipPath, CompressionLevel.Fastest, false, Encoding.UTF8);
+            using (var ar = ZipFile.Open(outputZipPath, ZipArchiveMode.Update))
+            {
+                var entry = ar.CreateEntry("pack.mcmeta");
+                using (var writer = new StreamWriter(entry.Open(), Encoding.UTF8))
+                {
+                    writer.WriteLine(
+                        "{\n" +
+                        "  \"pack\": {\n" +
+                        "    \"pack_format\": 2,\n" +
+                        "    \"description\": \"Made by MCResourcePackUtil (dev: yuma140902)\"\n" +
+                        "  }\n" +
+                        "}");
+                }
+            }
+        }
+    }
 }
